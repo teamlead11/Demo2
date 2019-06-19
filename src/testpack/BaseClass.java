398255a0407e13e60a8deaec76e4d055106f6236
Arunkumar92-cell <arunkumar.ece2013@gmail.com>
2019-06-19 11:55:55 +0530

    added string code

--- src/testpack/BaseClass.java
+++ src/testpack/BaseClass.java
@@ -7,6 +7,7 @@ public class BaseClass {
 		System.out.println("this is my base class");
 		BaseClass base = new BaseClass();
 		base.add(6, 7);
+		base.Arun();
 	}
 
 	public void add(int a, int b) {
@@ -24,4 +25,21 @@ public class BaseClass {
 	public void multi(int a, int b, int c) {
 		System.out.println(a * b * c);
 	}
+	public void Arun() {
+		String name = "Arun";
+		String res = "";
+		for (int i=name.length()-1;i>=0;i--) {
+			char ch=name.charAt(i);
+			res = res+ch;
+			
+		}
+	if(res.equals(name)) {
+		System.out.println("Palindrome");
+	}else {
+		System.out.println("Not Palindrome");
+	}
+	}
+
 }
+
+
