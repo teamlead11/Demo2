6094d4c773e92f93c24725e1908c0b78d5d4a7eb
latha418 <latha.madhavi418@gmail.com>
2019-06-19 16:27:59 +0530

    adding palindrome

--- src/testpack/BaseClass.java
+++ src/testpack/BaseClass.java
@@ -9,6 +9,7 @@ public class BaseClass {
 		base.add(6, 7);
 		base.Arun();
 		base.rama();
+		base.palindromeByMady();
 	}
 
 	public void add(int a, int b) {
@@ -52,6 +53,23 @@ public class BaseClass {
 			System.out.println("Palindrome");
 		}else {
 			System.out.println("Not Palindrome");
+		}
+	}
+	public void palindromeByMady() {
+		String s="MADHAVILATHA";
+		String res="";
+		int l = s.length();
+		
+		for(int i=(l-1);i>=0;i--) {
+			res=res+s.charAt(i);
+		}
+		System.out.println(res);
+		if(s.equals(res)) {
+			System.out.println("palindrome String");
+		}
+		else
+		{
+			System.out.println(s +" is not a palindrome String");
 		}
 
 	}
