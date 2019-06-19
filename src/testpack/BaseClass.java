7a84ca9cdd23f1e18bbbbdd59089a8a8f663bcfd
kishorerider91 <brillakishore@gmail.com>
2019-06-19 12:06:36 +0530

    added pallindrome

--- src/testpack/BaseClass.java
+++ src/testpack/BaseClass.java
@@ -24,4 +24,23 @@ public class BaseClass {
 	public void multi(int a, int b, int c) {
 		System.out.println(a * b * c);
 	}
+	{
+	 int num = 121, reversedInteger = 0, remainder, originalInteger;
+
+     originalInteger = num;
+
+     // reversed integer is stored in variable 
+     while( num != 0 )
+     {
+         remainder = num % 10;
+         reversedInteger = reversedInteger * 10 + remainder;
+         num  /= 10;
+     }
+
+
+     if (originalInteger == reversedInteger)
+         System.out.println(originalInteger + " is a palindrome.");
+     else
+         System.out.println(originalInteger + " is not a palindrome.");
+}
 }
