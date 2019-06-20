b6b03b5ee9dcaf85a144975a4bb38e3fa0a84118
praveenavarma <praveena1139@gmail.com>
2019-06-20 12:23:35 +0530

    added palindrome

--- src/testpack/BaseClass.java
+++ src/testpack/BaseClass.java
@@ -106,9 +106,27 @@ public class BaseClass {
 		else
 		{
 			System.out.println(s +" is not a palindrome String");
-		}
+		}}
 
+public void palindromByPraveena() {
+	
+	String rev="";
+	String s="mom";
+	for(int i=s.length()-1;i>=0;i--) {
+		rev=rev+s.charAt(i);
 	}
-}
+	if(s.equals(rev)) {
+		System.out.println("string is palindrome");
+	}
+	else {
+		System.out.println("string is not palindrome");
+	}
+}}	
+
+
+
+
+
+
 
 
